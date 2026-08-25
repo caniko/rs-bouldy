30c4b8970dfed39acea700ac4feb2aaf64518219
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-25 02:20:35 +0200

    build: use canonical GitHub inputs

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   inputs = {
-    rs-harbor.url = "git+https://github.com/caniko/rs-harbor.git?ref=trunk&rev=f209ddbca3fdbb0dc31fa3886ccc2ff7369c18ac";
+    rs-harbor.url = "git+https://github.com/caniko/harbor-rs.git?ref=trunk&rev=05cc4f162b55fa904b687db1821e2463fa813e50";
 
     rs-harbor-macos-sdk-pin.url = "git+ssh://git@github.com/caniko/harbor-macos-sdk-pin.git";
 
